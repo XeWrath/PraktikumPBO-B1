fd46b5164cdbe8eaa7e584e57f1c6bd44e3ff742
Muhammad Raihan Wardhana <117961909+XeWrath@users.noreply.github.com>
2024-03-05 11:22:29 +0700

    Add files via upload

---
+++ Pertemuan 2/Titik.java
@@ -0,0 +1,48 @@
+
+
+public class Titik {
+
+    // deklarasi atribut
+
+    private double absis;
+    private double ordinat;
+    private static int counterTitik;
+
+    // deklarasi konstruktor
+
+    public Titik(){
+        absis = 0 ;
+        ordinat= 0 ;
+        counterTitik++;
+
+    }
+
+    public Titik(double a, double b){
+        absis = a;
+        ordinat = b;
+        counterTitik++;
+    }
+
+    //deklarasi metode
+    public void setAbsis(double a){
+        absis = a;
+
+    }
+
+    public void setOrdinat(double o){
+        ordinat = o;
+
+    }
+
+    public double getAbsis(){
+        return absis;
+    }
+
+    public double getOrdinat(){
+        return ordinat;
+    }
+
+    public int getCounterTitik(){
+        return counterTitik;
+    }
+}
