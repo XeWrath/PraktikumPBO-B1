25e8b11f5113b30b7f4c36a7afad69e23f159c05
Muhammad Raihan Wardhana <117961909+XeWrath@users.noreply.github.com>
2024-03-09 22:23:04 +0700

    Add files via upload

---
+++ Pertemuan 3/Tugas Praktikum/JumlahPesananMelebihiStokException.java
@@ -0,0 +1,7 @@
+public class JumlahPesananMelebihiStokException extends Exception {
+    // TO DO 2: Buat konstructor dengan parameter message dan panggil konstruktor
+    // parent dengan keyword super
+    public JumlahPesananMelebihiStokException(String message){
+        super(message);
+    }
+}
